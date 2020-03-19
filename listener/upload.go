641f6230366cf4c76f20961d3bab725496ed053f
Michal Hornický <semtexzv@gmail.com>
2020-03-19 13:42:19 +0100

    Revert "added error mesage with repos list"
    This reverts commit d604fa492352d8d5b4d52a2ba1585fca56fcf57f.

--- listener/upload.go
+++ listener/upload.go
@@ -195,8 +195,6 @@ func updateSystemPlatform(tx *gorm.DB, inventoryID string, accountID int, host *
 
 	addedRepos, addedSysRepos, deletedSysRepos, err := updateRepos(tx, systemPlatform.ID, updatesReq.RepositoryList)
 	if err != nil {
-		utils.Log("repository_list", updatesReq.RepositoryList, "inventoryID", systemPlatform.ID).
-			Error("repos failed to insert")
 		return nil, errors.Wrap(err, "unable to update system repos")
 	}
 
